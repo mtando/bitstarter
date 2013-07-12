b0b9ccb3ad50f310e00c6c1d7f5bf0bcdd22c6b7
MTANDO <ubuntu@ip-172-31-29-108.us-west-2.compute.internal>
2013-07-12 05:23:59 +0000

    attempt1

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://vorsteiner.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,7 +39,13 @@ var assertFileExists = function(infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
-};
+}
+
+var cheerioFromURL = function(url){
+  rest.get(url).on('complete', function(data) {
+  return cheerio.load(data); // auto convert to object                                                                                              
+  });
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -55,18 +62,37 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlFileFromURL = function(url, checksfile){
+    $ = cheerioFromURL(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var i in checks){
+	var present = $(checks[i]).length > 0;
+	out[checks[ii] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url>', 'url of html file', URL_DEFAULT)
+	.parse(process.argv);
+
+    var checkJson;
+    if(program.file)
+	checkJson = checkHtmlFile(program.file, program.checks);
+    else
+	checkJson = checkHtmlFileFromURL(program.url, program.checks);
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
